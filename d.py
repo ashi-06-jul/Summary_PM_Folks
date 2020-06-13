4c854e50e167f6dc91a7b1a35c6b73009348607b
ashi-06-jul <36784332+ashi-06-jul@users.noreply.github.com>
2020-06-13 18:13:49 +0530

    update

--- d.py
+++ d.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 import json
 from iteration_utilities import duplicates
+from telegram.ext import Updater, CommandHandler
 
 with open('w.txt', "r", encoding='utf-8') as infile:
     outputData = { 'date': [], 'sender': [],'category': [], 'text': [] }
@@ -29,69 +30,49 @@ with open('w.txt', "r", encoding='utf-8') as infile:
     
     #for key in outputData['category'].keys(): 
      #   print(key)
-print('Scrolling through Whatsapp groups are a painful task. Let us give you a brief summary of what all happened on your PM Community group this week. ')
-def Repeat(x): 
-    _size = len(x) 
-    repeated = [] 
-    for i in range(_size): 
-        k = i + 1
-        for j in range(k, _size): 
-            if x[i] == x[j] and x[i] not in repeated: 
-                repeated.append(x[i]) 
-                print(x[i])
-                for i in range(_size):
-                  if x[i] == x[j]:
-                    print(outputData['text'][i])
-                  else:
-                    continue
-    return repeated 
-p=Repeat(outputData['category'])
-#print(p)
-for (k,v) in outputData['category'].items():
-    if v not in p:
-      print(outputData['category'][k])
-      print(outputData['text'][k])
-
-  #  for (k, v) in outputData['category'].items():
-  #   print("Key: " + str(k))
-  #  print("Value: " + str(v))
-           
-     #outputData = pd.DataFrame(outputData)
-    #outputData.to_json('output.json',indent=4)
-    #file = open ("w.txt",'r')
-    #f = file.readlines()
-    #d=str(f)
-    #dict_json=json.loads(d)
-   # print(type(dict_json))    
-
-#for i in outputData['category']:
- #   dict_json=json.loads(i)
-  #  print(dict_json)
-  
 
 
-    #file = open ("w.txt",'r')
-    #f = file.readlines()
-    #d=str(f)
-    #data=json.loads(d)
-    #print(type(data))
-    #for i in outputData['category']:
-    #   print(outputData['text'])
-      
- #file = open ("w.txt",'r')
-  #  regex=r"#[a-b][A-B]"  
-   # f = file.readlines()
-    #for s in f:
-     # category=s.split(' ')
-      #print(category)
-   # print('''Scrolling through Whatsapp groups are a painful task. 
-#Let us give you a brief summary of what all happened on your PM Community group this week. 
-#''')
- #   for i in outputData['sender']:
-    #for i in category:
-     # if i==i+1:
-      #  for j in outputData['text']:
-       #     print(i +':' +j)
-      #else:
-       # for j in outputData['text']:
-        #  print(i+':'+j)
+#p=Repeat(outputData['category'])
+
+def callback_alarm(bot, job):
+    def noRepeat():
+      for (k,v) in outputData['category'].items():
+        if v not in p:
+          print(outputData['category'][k])
+          print(outputData['text'][k])
+    def Repeat(x): 
+      _size = len(x) 
+      repeated = [] 
+      for i in range(_size): 
+          k = i + 1
+          for j in range(k, _size): 
+              if x[i] == x[j] and x[i] not in repeated: 
+                  bot.send_message(chat_id=job.context, text=x[i])
+                  repeated.append(x[i]) 
+                  print(x[i])
+                  for i in range(_size):
+                    if x[i] == x[j]:
+                      bot.send_message(chat_id=job.context, text=outputData['text'][i])
+                      print(outputData['text'][i])
+                    else:
+                      continue
+      return repeated 
+    p=Repeat(outputData['category'])
+    #bot.send_message(chat_id=job.context, text=p)
+    #bot.send_message(chat_id='1076770159', text='Wait for some min')
+
+def callback_timer(bot, update, job_queue):
+    bot.send_message(chat_id=update.message.chat_id,
+                      text='Starting!')
+    job_queue.run_repeating(callback_alarm, 5, context=update.message.chat_id)
+
+def stop_timer(bot, update, job_queue):
+    bot.send_message(chat_id=update.message.chat_id,
+                      text='Stoped!')
+    job_queue.stop()
+
+updater = Updater("your_bot_token")
+updater.dispatcher.add_handler(CommandHandler('start', callback_timer, pass_job_queue=True))
+updater.dispatcher.add_handler(CommandHandler('stop', stop_timer, pass_job_queue=True))
+
+updater.start_polling()
---
+++ queue1.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater, CommandHandler
+def callback_alarm(bot, job):
+    bot.send_message(chat_id=job.context, text='Alarm')
+    #bot.send_message(chat_id='1076770159', text='Wait for some min')
+
+def callback_timer(bot, update, job_queue):
+    bot.send_message(chat_id=update.message.chat_id,
+                      text='Starting!')
+    job_queue.run_repeating(callback_alarm, 5, context=update.message.chat_id)
+
+def stop_timer(bot, update, job_queue):
+    bot.send_message(chat_id=update.message.chat_id,
+                      text='Stoped!')
+    job_queue.stop()
+
+updater = Updater("1184324197:AAFChlWE5tU5X4lzHq-mQHvhxNIYisCqq1U")
+updater.dispatcher.add_handler(CommandHandler('start', callback_timer, pass_job_queue=True))
+updater.dispatcher.add_handler(CommandHandler('stop', stop_timer, pass_job_queue=True))
+
+updater.start_polling()
