b82b7cc376e9888d4ac9898f8fc4e18882217941
ashi-06-jul <36784332+ashi-06-jul@users.noreply.github.com>
2020-06-18 17:22:50 +0530

    update

--- d1.py
+++ d1.py
@@ -3,15 +3,23 @@ from datetime import datetime
 import pandas as pd
 
 with open('w.txt', "r", encoding='utf-8') as infile:
-    outputData = { 'date': [], 'sender': [],'questions':[], 'category': [],'links':[], 'text': [] }
+    regex_questions = r"[?]"
+    regex_category = r'(#\w+|#+)'
+    regex_links = r'(\Bhttps|\bhttps|\Ahttps)'
+    regex_text = r'(\s[^:]*?)$'
+    questions = []
+    category = []
+    links = []
+    text = []
     for line in infile:
-      if re.search ("[?]", line):
-        outputData['questions'].append(re.findall ("[?]", line) or "{undefined}")
-      elif re.search ("#\w+", line) or re.search("#+", line):
-        outputData['category'].append(re.findall ("#\w+", line) or re.findall("#+", line) or "{undefined}")
-      elif re.search("\Bhttps", line) or re.search("\bhttps", line) or re.search("\Ahttps", line):
-        outputData['links'].append(re.findall("\Bhttps", line) or re.findall("\bhttps", line) or re.findall ("\Ahttps", line) or "{undefined}")
-
+      if re.search(regex_questions, line):
+        questions.append(re.findall(regex_questions, line) or "{undefined}")
+      elif re.search(regex_category, line):
+        category.append(re.findall(regex_category, line) or "{undefined}")
+      elif re.search(regex_links, line):
+        links.append(re.findall(regex_links, line) or "{undefined}")
+      elif re.search(regex_text, line):
+        text.append(re.findall(regex_text, line) or "{undefined}")
         #matches = re.match(r'^(\d{1,2})\/(\d{1,2})\/(\d\d), (24:00|2[0-3]:[0-5][0-9]|[0-1][0-9]:[0-5][0-9]), ((\S[^:]*?): )?(.*), ("[?]"), (("#\w+")("#+")), (("\Bhttps")("\bhttps")("\Ahttps")), ((\S[^:]*?): )?(.*)$', line)
         #if matches:
          # outputData['date'].append(
@@ -31,7 +39,12 @@ with open('w.txt', "r", encoding='utf-8') as infile:
         #elif len(outputData['text']) > 0:
          # outputData['text'][-1] += "\n" + line[0:-1]
 
-    outputData = pd.DataFrame(outputData)
-    outputData.to_json('output.json',indent=6)
+   # outputData = pd.DataFrame(questions,category,links,text)
+    #outputData.to_json('output.json',indent=4)
+    print(questions)
+    print(category)
+    print(links)
+    print(text)
+
     #print(outputData['sender'])
 
